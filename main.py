4c449cb057641b2a142ced27f3d6d536580917d1
jjerry-k <jaeyeol2931@gmail.com>
2021-09-14 01:08:46 +0900

    Update

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from enum import Enum
 
-from typing import Optional
+from typing import Optional, List
 
 from pydantic import BaseModel
 
-from fastapi import FastAPI, Query, Request
+from fastapi import FastAPI, Query, Path, Body, Request
 from fastapi.responses import HTMLResponse
 from starlette.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
@@ -47,17 +47,80 @@ async def get_model(model_name: ModelName):
 def read_item(skip: int=0, limit: int=10):
     return [i for i in range(skip, limit)]
 
-@app.post("/pydantic")
-async def read_item(item: Item):
-    return item
-
-@app.get("/query")
-def read_item(q: Optional[str] = None):
+# Query Parameters
+@app.get("/query_params/")
+async def read_items(q: Optional[str] = Query("Default Value", title="Query Parameters", min_length=3, max_length=50)):
     results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    print(q)
+    if q:
+        results.update({"q": q})
+        results.update({"title": q.title()})
+    return results
+
+@app.get("/query_multi/")
+async def read_items(q: Optional[List[str]] = Query(None, title="Query Multiple Parameters")):
+    query_items = {"q": q}
+    return query_items
+
+# Path Parameters
+@app.get("/path_params/{item_id}")
+async def read_items(q: str, item_id: int = Path(..., title="The ID of the item to get", ge=1)):
+    results = {"item_id": item_id}
     if q:
         results.update({"q": q})
     return results
 
+# Declare Request Example Data
+
+class ExampleItem(BaseModel):
+    name: str
+    description: Optional[str] = None
+    price: float
+    tax: Optional[float] = None
+
+
+@app.put("/multiple_example/{item_id}")
+async def read_items(
+    *,
+    item_id: int,
+    item: ExampleItem = Body(
+        ...,
+        examples={
+            "normal": {
+                "summary": "A normal example",
+                "description": "A **normal** item works correctly.",
+                "value": {
+                    "name": "Foo",
+                    "description": "A very nice Item",
+                    "price": 35.4,
+                    "tax": 3.2,
+                },
+            },
+            "converted": {
+                "summary": "An example with converted data",
+                "description": "FastAPI can convert price `strings` to actual `numbers` automatically",
+                "value": {
+                    "name": "Bar",
+                    "price": "35.4",
+                },
+            },
+            "invalid": {
+                "summary": "Invalid data is rejected with an error",
+                "value": {
+                    "name": "Baz",
+                    "price": "thirty five point four",
+                },
+            },
+        },
+    ),
+):
+    results = {"item_id": item_id, "item": item}
+    return results
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item):
+    results = {"item_id": item_id, "item": item}
+    return results
 templates = Jinja2Templates(directory="templates")
 
 @app.get("/jinja/{id}", response_class=HTMLResponse)
